225ca344bb178d1c1bd3a4dcc10951a7b8c521cc
Stefanie <coralvar.st@gmail.com>
2025-03-14 13:41:52 -0500

    actualiza el app

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const campoNombre = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+
+let amigos = [];
+
+function agregarAmigo() {
+    const nombre = campoNombre.value.trim();
+    if (!nombre) return alert("Por favor, ingresa un nombre válido.");
+    amigos.push(nombre);
+    campoNombre.value = "";
+    actualizarLista();
+}
+
+function actualizarLista() {
+    listaAmigos.innerHTML = amigos.map(nombre => `<li>${nombre}</li>`).join("");
+}
