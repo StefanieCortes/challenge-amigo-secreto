705175982f6352847e4cab7507f78bc4f4740da2
Stefanie <coralvar.st@gmail.com>
2025-03-16 10:55:26 -0500

    actualiza el app

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ let amigos = [];
 
 function agregarAmigo() {
     const nombre = campoNombre.value.trim();
-    if (!nombre) return alert("Por favor, ingresa un nombre válido.");
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
     amigos.push(nombre);
     campoNombre.value = "";
     actualizarLista();
@@ -16,3 +19,15 @@ function agregarAmigo() {
 function actualizarLista() {
     listaAmigos.innerHTML = amigos.map(nombre => `<li>${nombre}</li>`).join("");
 }
+
+function sortearAmigo() {
+    if (!amigos.length === 0) {
+        alert("No hay nombres en la lista. Agrega al menos uno.");
+        return;
+    }
+    resultado.innerHTML = `<li>¡¡ El amigo secreto es: <strong>${amigos[Math.floor(Math.random() * amigos.length)]}</strong> !!!</li>`;
+}
+
+document.querySelector(".button-add").addEventListener("click", agregarAmigo);
+document.querySelector(".button-draw").addEventListener("click", sortearAmigo);
+campoNombre.addEventListener("keypress", e => { if (e.key === "Enter") agregarAmigo(); });
